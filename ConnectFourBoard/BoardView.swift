865c228e803ad295b821c6cad4d0f75f698805e2
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-10 12:48:02 -0500

    created function drawBoard() in BoardView

--- ConnectFourBoard/BoardView.swift
+++ ConnectFourBoard/BoardView.swift
@@ -14,6 +14,10 @@ class BoardView: UIView {
     let cellSide: CGFloat = 100
 
     override func draw(_ rect: CGRect) {
+        drawBoard()
+    }
+    
+    func drawBoard() {
         for j in 0 ..< 6 {
             for i in 0 ..< 7 {
                 let circle = UIBezierPath(arcCenter: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + CGFloat(j) * cellSide), radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
