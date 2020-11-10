a06409c797944d864a00ab86dd7ab818cf7d39ef
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-10 13:52:42 -0500

    created a set of Conn4Piece objects

--- Connect4Tests/SetTests.swift
+++ Connect4Tests/SetTests.swift
@@ -10,12 +10,25 @@ import XCTest
 
 class SetTests: XCTestCase {
     
+    func testConn4PieceArray() {
+        var pieces: [Conn4Piece] = []
+        
+        pieces.append(Conn4Piece(col: 0, row: 0, player: Conn4Player.red))
+        pieces.append(Conn4Piece(col: 0, row: 0, player: Conn4Player.red))
+        
+        XCTAssertEqual(2, pieces.count)
+    }
+    
     func testConn4PieceSet() {
         var pieces: Set<Conn4Piece> = []
         
         pieces.insert(Conn4Piece(col: 0, row: 0, player: Conn4Player.red))
+        pieces.insert(Conn4Piece(col: 0, row: 0, player: Conn4Player.red))
         
         XCTAssertEqual(1, pieces.count)
+        
+        pieces.insert(Conn4Piece(col: 1, row: 0, player: Conn4Player.red))
+        XCTAssertEqual(2, pieces.count)
     }
     
     func testStringSet() {
--- ConnectFourBoard/BoardView.swift
+++ ConnectFourBoard/BoardView.swift
@@ -12,21 +12,38 @@ class BoardView: UIView {
     let originY: CGFloat = 121
     let radius: CGFloat = 40
     let cellSide: CGFloat = 100
+    
+    var shadowPiecesBox: Set<Conn4Piece> = []
+    
 
     override func draw(_ rect: CGRect) {
+        // tmp code
+        shadowPiecesBox.insert(Conn4Piece(col: 0, row: 0, player: .red))
+        
         drawBoard()
+        drawPieces()
+    }
+    
+    func drawPieces() {
+        for piece in shadowPiecesBox {
+            drawCircleAt(col: piece.col, row: piece.row, color: piece.player == Conn4Player.red ? UIColor.red : UIColor.yellow)
+        }
     }
     
     func drawBoard() {
-        for j in 0 ..< 6 {
-            for i in 0 ..< 7 {
-                let circle = UIBezierPath(arcCenter: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + CGFloat(j) * cellSide), radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-                
-                UIColor.white.setFill()
-                circle.fill()
-                
-                circle.stroke()
+        for row in 0 ..< 6 {
+            for col in 0 ..< 7 {
+                drawCircleAt(col: col, row: row, color: UIColor.white)
             }
         }
+    }
+    
+    func drawCircleAt(col: Int, row: Int, color: UIColor) {
+        let circle = UIBezierPath(arcCenter: CGPoint(x: originX + CGFloat(col) * cellSide, y: originY + CGFloat(row) * cellSide), radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        color.setFill()
+        circle.fill()
+        
+        circle.stroke()
     }
 }
