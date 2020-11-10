3f1723b2994ce5a482eefe35bdc70d8e3da8e43c
Golden Thumb <zhijunsheng@mbp2020.local>
2020-11-10 12:43:48 -0500

    migrated existing connect4 iOS project to github

---
+++ ConnectFourBoard/BoardView.swift
@@ -0,0 +1,28 @@
+//
+//  BoardView.swift
+//  ConnectFourBoard
+//
+//  Created by Golden Thumb on 2020-10-29.
+//
+
+import UIKit
+
+class BoardView: UIView {
+    let originX: CGFloat = 66
+    let originY: CGFloat = 121
+    let radius: CGFloat = 40
+    let cellSide: CGFloat = 100
+
+    override func draw(_ rect: CGRect) {
+        for j in 0 ..< 6 {
+            for i in 0 ..< 7 {
+                let circle = UIBezierPath(arcCenter: CGPoint(x: originX + CGFloat(i) * cellSide, y: originY + CGFloat(j) * cellSide), radius: radius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+                
+                UIColor.white.setFill()
+                circle.fill()
+                
+                circle.stroke()
+            }
+        }
+    }
+}
